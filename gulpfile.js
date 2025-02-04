5286d53b5de7c8d407a21a121f673d013adfe09a
Renannporto <144285350+Renannporto@users.noreply.github.com>
2025-02-04 17:10:08 +0000

    criando o hero e importando a fonte avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-const gulp = require("gulp");
+const gulp = require("gulp");//essas const é para importar os pluggins que instalamos.
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
 function styles(){
     return gulp.src("./src/styles/*.scss")
@@ -7,7 +8,14 @@ function styles(){
     .pipe(gulp.dest("./dist/css"));
 }  
 
-exports.default = styles;
+function images(){
+    return gulp.src("./src/images/**/*") //os asteristicos é para acessar a pasta SRC e pegar todas as imagens que estiverem la dentro.
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));
+}  
+
+exports.default = gulp.parallel(styles,images);
+
 exports.watch = function() {
     gulp.watch("./src/styles/*.scss", gulp.parallel(styles))
 }
